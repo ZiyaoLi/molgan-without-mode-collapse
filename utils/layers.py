397486da99d39e465fdb365f8f14c8d56f736182
ZiyaoLi <leeeezy@126.com>
2020-05-20 17:08:43 +0800

    gat implemented (untested yet)

--- utils/layers.py
+++ utils/layers.py
@@ -31,6 +31,58 @@ def graph_convolution_layer(inputs, units, training, activation=None, dropout_ra
     return output
 
 
+def masked_softmax(logits, masks):
+    logits -= tf.reduce_max(logits, axis=-1, keep_dims=True)   # (b, nb, n, n)
+    energy = tf.exp(logits) * masks
+    output = energy / tf.reduce_sum(energy, axis=-1, keep_dims=True)
+    return output
+
+
+def attn_head(queries, keys, values, adj, units=None, residual=False):
+    f_q = tf.stack([tf.layers.dense(queries, 1) for _ in range(adj.shape[1])], 1)     # (b, nb, n, 1)
+    f_k = tf.stack([tf.layers.dense(keys, 1) for _ in range(adj.shape[1])], 1)        # (b, nb, n, 1)
+
+    f_v = tf.stack([tf.layers.dense(values, units) for _ in range(adj.shape[1])], 1)  # (b, nb, n, u)
+
+    logits = f_q + tf.transpose(f_k, (0, 1, 3, 2))  # (b, nb, n, n)
+    coefs = masked_softmax(logits, adj)             # (b, nb, n, n)
+
+    outputs = tf.matmul(coefs, f_v)                 # (b, nb, n, n)
+
+    if residual:
+        outputs += f_v
+
+    return outputs
+
+
+def gat_layer(inputs, units, n_heads, training, activation=None, dropout_rate=0.):
+    '''
+    :param inputs: (adjacency_tensor, hidden_tensor, node_tensor).
+    adj with shape (b, n, n, c); hidden & node with shape (b, n, d)
+    :param units:
+    :param n_heads: num of attn heads
+    :param training:
+    :param activation:
+    :param dropout_rate:
+    :return:
+    '''
+    # hidden tensors are always None in the example.
+    adjacency_tensor, hidden_tensor, node_tensor = inputs
+    adj = tf.transpose(adjacency_tensor[:, :, :, 1:],   # ignore 0-indexed edges.
+                       (0, 3, 1, 2))                    # (b, c, n, n) = (?, 4, 9, 9)
+
+    annotations = tf.concat((hidden_tensor, node_tensor), -1) \
+        if hidden_tensor is not None else node_tensor
+
+    output = tf.concat([attn_head(annotations, annotations, annotations, adj, units=units // n_heads)
+                        for _ in range(n_heads)], axis=-1)
+    output = tf.reduce_sum(output, 1) + tf.layers.dense(inputs=annotations, units=output.shape[-1])  # res
+    output = activation(output) if activation is not None else output
+    output = tf.layers.dropout(output, dropout_rate, training=training)
+
+    return output
+
+
 def graph_aggregation_layer(inputs, units, training, activation=None, dropout_rate=0.):
     '''
     Readout fn. defined in GG-NN
@@ -67,5 +119,15 @@ def multi_graph_convolution_layers(inputs, units, training, activation=None, dro
         hidden_tensor = graph_convolution_layer(inputs=(adjacency_tensor, hidden_tensor, node_tensor),
                                                 units=u, activation=activation, dropout_rate=dropout_rate,
                                                 training=training)
+
+    return hidden_tensor
+
+
+def multi_gat_layers(inputs, units, n_heads, training, activation=None, dropout_rate=0.):
+    adjacency_tensor, hidden_tensor, node_tensor = inputs
+    for u in units:
+        hidden_tensor = gat_layer(inputs=(adjacency_tensor, hidden_tensor, node_tensor),
+                                  units=u, n_heads=n_heads, activation=activation, dropout_rate=dropout_rate,
+                                  training=training)
 
     return hidden_tensor
