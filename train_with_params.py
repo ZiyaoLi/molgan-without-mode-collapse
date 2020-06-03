41f5ba207f61651e5563814e8541f85e44b495ec
ZiyaoLi <leeeezy@126.com>
2020-06-03 13:08:18 +0800

    slurm modified to train_with_params

--- train_with_params.py
+++ train_with_params.py
@@ -9,10 +9,6 @@ from models import *
 
 from optimizers.gan import GraphGANOptimizer
 
-from example import train_feed_dict, train_fetch_dict, \
-    eval_feed_dict, eval_fetch_dict, test_feed_dict, test_fetch_dict, \
-    reward, _eval_update, _test_update
-
 DECODER_UNITS = (128, 256, 512)                   # z = Dense(z, dim=units_k)^{(k)}
 DISCRIM_UNITS = ((64, 32), 128, (128,))           # (GCN units, Readout units, MLP units)
 batch_dim = 32
@@ -29,6 +25,159 @@ data = SparseMolecularDataset()
 data.load('data/gdb9_9nodes.sparsedataset')
 
 steps = (len(data) // batch_dim)
+
+
+def train_fetch_dict(i, steps, epoch, epochs, min_epochs, model, optimizer):
+    la = 1 if epoch < epochs / 2 else LA
+    a = [optimizer.train_step_G] if i % n_critic == 0 else [optimizer.train_step_D]
+    b = [optimizer.train_step_V] if i % n_critic == 0 and la < 1 else []
+    return a + b
+
+
+def train_feed_dict(i, steps, epoch, epochs, min_epochs, model, optimizer, batch_dim):
+    mols, _, _, a, x, _, _, _, _ = data.next_train_batch(batch_dim)
+    embeddings = model.sample_z(batch_dim)
+
+    la = 1 if epoch < epochs / 2 else LA
+
+    if la < 1:
+
+        if i % n_critic == 0:
+            rewardR = reward(mols)
+
+            n, e = session.run([model.nodes_gumbel_argmax, model.edges_gumbel_argmax],
+                               feed_dict={model.training: False, model.embeddings: embeddings})
+            n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
+            mols = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+
+            rewardF = reward(mols)
+
+            feed_dict = {model.edges_labels: a,
+                         model.nodes_labels: x,
+                         model.embeddings: embeddings,
+                         model.rewardR: rewardR,
+                         model.rewardF: rewardF,
+                         model.training: True,
+                         model.dropout_rate: dropout,
+                         optimizer.la: la if epoch > 0 else 1.0}
+
+        else:
+            feed_dict = {model.edges_labels: a,
+                         model.nodes_labels: x,
+                         model.embeddings: embeddings,
+                         model.training: True,
+                         model.dropout_rate: dropout,
+                         optimizer.la: la if epoch > 0 else 1.0}
+    else:
+        feed_dict = {model.edges_labels: a,
+                     model.nodes_labels: x,
+                     model.embeddings: embeddings,
+                     model.training: True,
+                     model.dropout_rate: dropout,
+                     optimizer.la: 1.0}
+
+    return feed_dict
+
+
+def eval_fetch_dict(i, epochs, min_epochs, model, optimizer):
+    return {'loss D': optimizer.loss_D, 'loss G': optimizer.loss_G,
+            'loss RL': optimizer.loss_RL, 'loss V': optimizer.loss_V,
+            'la': optimizer.la}
+
+
+def eval_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
+    mols, _, _, a, x, _, _, _, _ = data.next_validation_batch()
+    embeddings = model.sample_z(a.shape[0])
+
+    rewardR = reward(mols)
+
+    n, e = session.run([model.nodes_gumbel_argmax, model.edges_gumbel_argmax],
+                       feed_dict={model.training: False, model.embeddings: embeddings})
+    n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
+    mols = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+
+    rewardF = reward(mols)
+
+    feed_dict = {model.edges_labels: a,
+                 model.nodes_labels: x,
+                 model.embeddings: embeddings,
+                 model.rewardR: rewardR,
+                 model.rewardF: rewardF,
+                 model.training: False}
+    return feed_dict
+
+
+def test_fetch_dict(model, optimizer):
+    return {'loss D': optimizer.loss_D, 'loss G': optimizer.loss_G,
+            'loss RL': optimizer.loss_RL, 'loss V': optimizer.loss_V,
+            'la': optimizer.la}
+
+
+def test_feed_dict(model, optimizer, batch_dim):
+    mols, _, _, a, x, _, _, _, _ = data.next_test_batch()
+    embeddings = model.sample_z(a.shape[0])
+
+    rewardR = reward(mols)
+
+    n, e = session.run([model.nodes_gumbel_argmax, model.edges_gumbel_argmax],
+                       feed_dict={model.training: False, model.embeddings: embeddings})
+    n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
+    mols = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+
+    rewardF = reward(mols)
+
+    feed_dict = {model.edges_labels: a,
+                 model.nodes_labels: x,
+                 model.embeddings: embeddings,
+                 model.rewardR: rewardR,
+                 model.rewardF: rewardF,
+                 model.training: False}
+    return feed_dict
+
+
+def reward(mols):
+    rr = 1.
+    for m in ('logp,sas,qed,unique' if metric == 'all' else metric).split(','):
+
+        if m == 'np':
+            rr *= MolecularMetrics.natural_product_scores(mols, norm=True)
+        elif m == 'logp':
+            rr *= MolecularMetrics.water_octanol_partition_coefficient_scores(mols, norm=True)
+        elif m == 'sas':
+            rr *= MolecularMetrics.synthetic_accessibility_score_scores(mols, norm=True)
+        elif m == 'qed':
+            rr *= MolecularMetrics.quantitative_estimation_druglikeness_scores(mols, norm=True)
+        elif m == 'novelty':
+            rr *= MolecularMetrics.novel_scores(mols, data)
+        elif m == 'dc':
+            rr *= MolecularMetrics.drugcandidate_scores(mols, data)
+        elif m == 'unique':
+            rr *= MolecularMetrics.unique_scores(mols)
+        elif m == 'diversity':
+            rr *= MolecularMetrics.diversity_scores(mols, data)
+        elif m == 'validity':
+            rr *= MolecularMetrics.valid_scores(mols)
+        else:
+            raise RuntimeError('{} is not defined as a metric'.format(m))
+
+    return rr.reshape(-1, 1)
+
+
+def _eval_update(i, epochs, min_epochs, model, optimizer, batch_dim, eval_batch):
+    mols = samples(data, model, session, model.sample_z(n_samples), sample=True)
+    m0, m1 = all_scores(mols, data, norm=True)
+    m0 = {k: np.array(v)[np.nonzero(v)].mean() for k, v in m0.items()}
+    m0.update(m1)
+    return m0
+
+
+def _test_update(model, optimizer, batch_dim, test_batch):
+    mols = samples(data, model, session, model.sample_z(n_samples), sample=True)
+    m0, m1 = all_scores(mols, data, norm=True)
+    m0 = {k: np.array(v)[np.nonzero(v)].mean() for k, v in m0.items()}
+    m0.update(m1)
+    return m0
+
 
 
 def Argparser():
