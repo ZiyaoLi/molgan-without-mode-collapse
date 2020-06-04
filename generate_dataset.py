365080c40abb7bdad485c0fbfc0a62cb071fea1a
ZiyaoLi <leeeezy@126.com>
2020-06-05 01:48:25 +0800

    conditional GAN trainer

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,9 +1,9 @@
 from utils.sparse_molecular_dataset import SparseMolecularDataset, SparseMolecularDatasetWithRewards
 
 if __name__ == '__main__':
-    # data = SparseMolecularDataset()
-    # data.generate('data/gdb9.sdf', filters=lambda x: x.GetNumAtoms() <= 9)  ## QM9 filter
-    # data.save('data/gdb9_9nodes.sparsedataset')
+    data = SparseMolecularDataset()
+    data.generate('data/gdb9.sdf', filters=lambda x: x.GetNumAtoms() <= 9)  ## QM9 filter
+    data.save('data/gdb9_9nodes.sparsedataset')
 
     data = SparseMolecularDatasetWithRewards()
     data.generate('data/gdb9.sdf', filters=lambda x: x.GetNumAtoms() <= 9, size=100)
