801062efb0aca29f7fed67a3eb44b082f8895b30
ZiyaoLi <leeeezy@126.com>
2020-06-05 01:48:43 +0800

    conditional GAN trainer

--- generate_dataset.py
+++ generate_dataset.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     data.generate('data/gdb9.sdf', filters=lambda x: x.GetNumAtoms() <= 9, size=100)
     data.save('data/gdb9_9nodes.rewarddataset')
 
+
     # data = SparseMolecularDataset()
     # data.generate('data/qm9_5k.smi', validation=0.00021, test=0.00021)  # , filters=lambda x: x.GetNumAtoms() <= 9)
     # data.save('data/qm9_5k.sparsedataset')
