6e8ad201948d074a4f7cbb970cc9d9f569625934
ZiyaoLi <leeeezy@126.com>
2020-06-07 18:11:42 +0800

    better organization

--- conditional_train.py
+++ conditional_train.py
@@ -1,25 +1,36 @@
-import tensorflow as tf
 
-from utils.sparse_molecular_dataset import SparseMolecularDatasetWithRewards
-from utils.trainer import Trainer
 from utils.utils import *
+from utils.trainer import Trainer
+from utils.sparse_molecular_dataset import SparseMolecularDatasetWithRewards
 
-from models.gan import *
 from models import *
-
+from models.gan import *
 from optimizers.gan import GraphGANOptimizer
+
+
+MODELS = {  # Model, discriminator, use_batch_discr, N_CRITIC
+    'molgan': (GraphGANModel, encoder_rgcn, False, 5),
+    'molgan+batch': (GraphGANModel, encoder_rgcn, True, 3),
+    'pmax': (PacGANModel, encoder_rgcn, False, 3),
+    'pe1': (PacElas1Model, encoder_rgcn, False, 5),
+    'pe2': (PacElas2Model, encoder_rgcn, False, 5),
+    'pem': (PacMeanElas2Model, encoder_rgcn, False, 3),
+    'pmerge': (PacStatsMergeModel, encoder_rgcn, False, 3),
+    'gat': (GraphGANModel, encoder_gat, False, 5),
+    'flat_gat': (GraphGANModel, encoder_flat_gat, False, 5),
+}
 
 DECODER_UNITS = (128, 256, 512)                   # z = Dense(z, dim=units_k)^{(k)}
 DISCRIM_UNITS = ((64, 32), 128, (128,))           # (GCN units, Readout units, MLP units)
 Z_DIM = 32
 
 METRIC = 'validity,qed'  # candidates in 'np,logp,sas,qed,novelty,dc,unique,diversity,validity'
-BATCH_DIM = 4
-LA = 0.05
-DROPOUT = 0
-N_CRITIC = 5
+LA = 0.05                # will be overridden
+N_CRITIC = 5             # will be overridden
+BATCH_DIM = 32
+DROPOUT = 0.0
 N_SAMPLES = 5000
-EPOCHS = 10
+EPOCHS = 72
 SAVE_EVERY = None
 
 
@@ -30,42 +41,34 @@ def train_fetch_dict(i, steps, epoch, epochs, min_epochs, model, optimizer):
 
 
 def train_feed_dict(i, steps, epoch, epochs, min_epochs, model, optimizer, batch_dim):
-    stage = 0 if epoch < epochs / 2 else 1
+    phase = 'pretrain' if epoch < epochs / 2 else 'reward'
 
-    if stage == 0:
+    if phase == 'pretrain':
         molsR, _, _, a, x, _, _, _, _ = data.next_train_batch(batch_dim)
-    else:
+    else:  # use conditional training in the second phase.
         molsR, _, _, a, x, _, _, _, _ = data.next_cond_train_batch(batch_dim)
 
     z_embeds = model.sample_z(batch_dim)
 
-    la = 1 if stage == 0 else LA
+    lam = 1 if phase == 'pretrain' else LA
 
-    if i % N_CRITIC == 0 and LA < 1 and la == 1:  # reward net shall be optimized
+    feed_dict = {model.edges_labels: a,
+                 model.nodes_labels: x,
+                 model.embeddings: z_embeds,
+                 model.training: True,
+                 model.dropout_rate: DROPOUT,
+                 optimizer.la: lam}
+
+    if i % N_CRITIC == 0 and LA < 1:   # that reward net shall be optimized
         rewardR = reward(molsR, METRIC, data)
-        # generate fake mols to train the reward net (as augmentation; determining validity et al)
         n, e = session.run([model.nodes_gumbel_argmax, model.edges_gumbel_argmax],
                            feed_dict={model.training: False, model.embeddings: z_embeds})
         n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
         molsF = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
         rewardF = reward(molsF, METRIC, data)
 
-        feed_dict = {model.edges_labels: a,
-                     model.nodes_labels: x,
-                     model.embeddings: z_embeds,
-                     model.rewardR: rewardR,
-                     model.rewardF: rewardF,
-                     model.training: True,
-                     model.dropout_rate: DROPOUT,
-                     optimizer.la: la}
-
-    else:
-        feed_dict = {model.edges_labels: a,
-                     model.nodes_labels: x,
-                     model.embeddings: z_embeds,
-                     model.training: True,
-                     model.dropout_rate: DROPOUT,
-                     optimizer.la: 1.0}
+        feed_dict.update({model.rewardR: rewardR,
+                          model.rewardF: rewardF})
 
     return feed_dict
 
@@ -77,11 +80,11 @@ def eval_fetch_dict(i, epochs, min_epochs, model, optimizer):
 
 
 def eval_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
-    stage = 0 if i < epochs / 2 else 1
+    phase = 'pretrain' if i < epochs / 2 else 'reward'
 
-    if stage == 0:
+    if phase == 'pretrain':
         molsR, _, _, a, x, _, _, _, _ = data.next_validation_batch(batch_dim)
-    else:
+    else:  # use conditional training in the second phase.
         molsR, _, _, a, x, _, _, _, _ = data.next_cond_validation_batch(batch_dim)
 
     z_embeds = model.sample_z(a.shape[0])
@@ -91,10 +94,9 @@ def eval_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
                        feed_dict={model.training: False, model.embeddings: z_embeds})
     n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
     molsF = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+    rewardF = reward(molsR, METRIC, data)
 
     record_mols_and_smiles(molsF, i)
-
-    rewardF = reward(molsR, METRIC, data)
 
     feed_dict = {model.edges_labels: a,
                  model.nodes_labels: x,
@@ -102,6 +104,7 @@ def eval_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
                  model.rewardR: rewardR,
                  model.rewardF: rewardF,
                  model.training: False}
+
     return feed_dict
 
 
@@ -118,11 +121,11 @@ _test_update = _eval_update
 
 
 def test_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
-    stage = 0 if i < epochs / 2 else 1
+    phase = 'pretrain' if i < epochs / 2 else 'reward'
 
-    if stage == 0:
+    if phase == 'pretrain':
         molsR, _, _, a, x, _, _, _, _ = data.next_test_batch(batch_dim)
-    else:
+    else:  # use conditional training in the second phase.
         molsR, _, _, a, x, _, _, _, _ = data.next_cond_test_batch(batch_dim)
 
     z_embeds = model.sample_z(a.shape[0])
@@ -132,10 +135,9 @@ def test_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
                        feed_dict={model.training: False, model.embeddings: z_embeds})
     n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
     molsF = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+    rewardF = reward(molsR, METRIC, data)
 
     record_mols_and_smiles(molsF, i)
-
-    rewardF = reward(molsR, METRIC, data)
 
     feed_dict = {model.edges_labels: a,
                  model.nodes_labels: x,
@@ -143,37 +145,42 @@ def test_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
                  model.rewardR: rewardR,
                  model.rewardF: rewardF,
                  model.training: False}
+
     return feed_dict
 
 
 def record_mols_and_smiles(mols, epoch, draw=True):
-    smiles = list(map(lambda x: Chem.MolToSmiles(x) if MolecularMetrics.valid_lambda(x) else '', mols))
-    valid_unique_smiles = list(set([s for s in smiles if len(s) and '*' not in s and '.' not in s]))
+
+    smiles = list(map(lambda x: Chem.MolToSmiles(x) if MolecularMetrics.valid_lambda_special(x) else '',
+                      mols))
+
+    valid_unique_smiles = list(set([s for s in smiles if len(s) > 0]))
+
+    # introduce randomness for sure and then sample.
     np.random.shuffle(smiles)
     np.random.shuffle(valid_unique_smiles)
-
     smiles = smiles[:30]
     valid_unique_smiles = valid_unique_smiles[:30]
 
     with open(save_dir + '/sample.smiles', 'a') as fh:
-        fh.write('START Epoch %03d:\n' % epoch)
+        fh.write('\nSTART Epoch %03d:\n' % epoch)
         fh.write('\n'.join(smiles))
-        fh.write('END Epoch %03d:\n' % epoch)
+        fh.write('\nEND Epoch %03d:\n' % epoch)
     with open(save_dir + '/val_unique.smiles', 'a') as fh:
-        fh.write('START Epoch %03d:\n' % epoch)
+        fh.write('\nSTART Epoch %03d:\n' % epoch)
         fh.write('\n'.join(valid_unique_smiles))
-        fh.write('END Epoch %03d:\n' % epoch)
+        fh.write('\nEND Epoch %03d:\n' % epoch)
 
-    draw_mols = list(map(lambda x: Chem.MolFromSmiles(x) if len(x) else Chem.MolFromSmiles('C'), smiles))
-    draw_vu_mols = list(map(lambda x: Chem.MolFromSmiles(x), valid_unique_smiles))
+    mols = list(map(lambda x: Chem.MolFromSmiles(x), smiles))  # use CH4 for placeholder.
+    valid_unique_mols = list(map(lambda x: Chem.MolFromSmiles(x), valid_unique_smiles))
 
     if draw:
         from rdkit.Chem import Draw
-        if draw and len(draw_mols):
-            img = Draw.MolsToGridImage(draw_mols, molsPerRow=5, subImgSize=(300, 300))
+        if len(mols):
+            img = Draw.MolsToGridImage(mols, molsPerRow=5, subImgSize=(300, 300))
             img.save(save_dir + "/samples_%03d.png" % epoch)
-        if len(draw_vu_mols):
-            img = Draw.MolsToGridImage(draw_vu_mols, molsPerRow=5, subImgSize=(300, 300))
+        if len(valid_unique_mols):
+            img = Draw.MolsToGridImage(valid_unique_mols, molsPerRow=5, subImgSize=(300, 300))
             img.save(save_dir + "/val_unique_%03d.png" % epoch)
 
 
@@ -185,20 +192,9 @@ def Argparser():
     parser.add_argument('-m', '--model', type=str, help='model name.')
     parser.add_argument('-r', '--replicas', type=int, default=1, help='number of replicas of training.')
     parser.add_argument('--lam', type=float, default=0.05, help='lambda')
+    parser.add_argument('--cond_rate', type=float, default=0.30, help='top r proportion of nodes w.r.t. rewards'
+                                                                      'used in conditional training.')
     return parser
-
-
-MODELS = {  # Model, Discr, use_batch_discr, n_critics
-    'molgan': (GraphGANModel, encoder_rgcn, False, 5),
-    'molgan+batch': (GraphGANModel, encoder_rgcn, True, 3),
-    'pmax': (PacGANModel, encoder_rgcn, False, 3),
-    'pe1': (PacElas1Model, encoder_rgcn, False, 5),
-    'pe2': (PacElas2Model, encoder_rgcn, False, 5),
-    'pem': (PacMeanElas2Model, encoder_rgcn, False, 3),
-    'pmerge': (PacStatsMergeModel, encoder_rgcn, False, 3),
-    # 'gat': (GraphGANModel, encoder_gat, False),
-    # 'flat_gat': (GraphGANModel, encoder_flat_gat, False),
-}
 
 
 if __name__ == '__main__':
@@ -207,40 +203,44 @@ if __name__ == '__main__':
 
     LA = args.lam
 
-    Model, Discr, batch_discr, N_CRITIC = MODELS[args.model]
+    Model, discriminator, use_batch_discr, N_CRITIC = MODELS[args.model]
 
     for i in range(args.replicas):
 
-        tf.reset_default_graph()  # rebuild comp. graph across replicas
+        tf.reset_default_graph()  # rebuild graph across replicas
 
+        # load data.
         data = SparseMolecularDatasetWithRewards()
-        data.load('data/gdb9_9nodes.rewarddataset', conditional_rate=1)
+        data.load('data/gdb9_9nodes.rewarddataset',
+                  metric=METRIC, conditional_rate=args.cond_rate)
 
-        steps = (len(data) // BATCH_DIM)
+        steps = (len(data) // BATCH_DIM)  # although weird (should be data.train_count // BATCH_DIM),
+                                          # we keep this setting the same as MolGAN.
 
+        # build result directory.
         save_dir = args.name + ('_%02d' % i if args.replicas > 1 else '')
         import os
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
-        # model
+        # model.
         model = Model(data.vertexes, data.bond_num_types, data.atom_num_types, Z_DIM,
                       decoder_units=DECODER_UNITS,
                       discriminator_units=DISCRIM_UNITS,
                       decoder=decoder_adj,
-                      discriminator=Discr,
+                      discriminator=discriminator,
                       soft_gumbel_softmax=False,
                       hard_gumbel_softmax=False,
-                      batch_discriminator=batch_discr)
+                      batch_discriminator=use_batch_discr)
 
-        # optimizer
+        # optimizer.
         optimizer = GraphGANOptimizer(model, learning_rate=1e-3, feature_matching=False)
 
-        # session
+        # session.
         session = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
         session.run(tf.global_variables_initializer())
 
-        # trainer
+        # trainer.
         trainer = Trainer(model, optimizer, session)
 
         print('Parameters: {}'.format(np.sum([np.prod(e.shape) for e in session.run(tf.trainable_variables())])))
@@ -252,8 +252,8 @@ if __name__ == '__main__':
                       _eval_update=_eval_update, _test_update=_test_update,
                       save_every=SAVE_EVERY, directory=save_dir)
 
+        # clean up
         session.close()
-
         del model, optimizer, trainer, data
 
 
--- molgan_train.py
+++ molgan_train.py
@@ -1,66 +1,71 @@
-import tensorflow as tf
 
-from utils.sparse_molecular_dataset import SparseMolecularDataset
-from utils.trainer import Trainer
 from utils.utils import *
+from utils.trainer import Trainer
+from utils.sparse_molecular_dataset import SparseMolecularDataset
 
-from models.gan import *
 from models import *
-
+from models.gan import *
 from optimizers.gan import GraphGANOptimizer
+
+
+MODELS = {  # Model, discriminator, use_batch_discr, N_CRITIC
+    'molgan': (GraphGANModel, encoder_rgcn, False, 5),
+    'molgan+batch': (GraphGANModel, encoder_rgcn, True, 3),
+    'pmax': (PacGANModel, encoder_rgcn, False, 3),
+    'pe1': (PacElas1Model, encoder_rgcn, False, 5),
+    'pe2': (PacElas2Model, encoder_rgcn, False, 5),
+    'pem': (PacMeanElas2Model, encoder_rgcn, False, 3),
+    'pmerge': (PacStatsMergeModel, encoder_rgcn, False, 3),
+    'gat': (GraphGANModel, encoder_gat, False, 5),
+    'flat_gat': (GraphGANModel, encoder_flat_gat, False, 5),
+}
 
 DECODER_UNITS = (128, 256, 512)                   # z = Dense(z, dim=units_k)^{(k)}
 DISCRIM_UNITS = ((64, 32), 128, (128,))           # (GCN units, Readout units, MLP units)
 Z_DIM = 32
 
 METRIC = 'validity,qed'  # candidates in 'np,logp,sas,qed,novelty,dc,unique,diversity,validity'
+LA = 0.05                # will be overridden
+N_CRITIC = 5             # will be overridden
 BATCH_DIM = 32
-LA = 0.05
-DROPOUT = 0
-N_CRITIC = 5
+DROPOUT = 0.0
 N_SAMPLES = 5000
-EPOCHS = 64
+EPOCHS = 72
 SAVE_EVERY = None
 
 
 def train_fetch_dict(i, steps, epoch, epochs, min_epochs, model, optimizer):
-    la = 1 if epoch < epochs / 2 else LA
     gan_ops = [optimizer.train_step_G] if i % N_CRITIC == 0 else [optimizer.train_step_D]
-    rwd_ops = [optimizer.train_step_V] if i % N_CRITIC == 0 and LA < 1 and la == 1 else []
+    rwd_ops = [optimizer.train_step_V] if i % N_CRITIC == 0 and LA < 1 else []
     return gan_ops + rwd_ops
 
 
 def train_feed_dict(i, steps, epoch, epochs, min_epochs, model, optimizer, batch_dim):
+    phase = 'pretrain' if epoch < epochs / 2 else 'reward'
+
     molsR, _, _, a, x, _, _, _, _ = data.next_train_batch(batch_dim)
+
     z_embeds = model.sample_z(batch_dim)
 
-    la = 1 if epoch < epochs / 2 else LA
+    lam = 1 if phase == 'pretrain' else LA
 
-    if i % N_CRITIC == 0 and LA < 1 and la == 1:  # reward net shall be optimized
+    feed_dict = {model.edges_labels: a,
+                 model.nodes_labels: x,
+                 model.embeddings: z_embeds,
+                 model.training: True,
+                 model.dropout_rate: DROPOUT,
+                 optimizer.la: lam}
+
+    if i % N_CRITIC == 0 and LA < 1:   # that reward net shall be optimized
         rewardR = reward(molsR, METRIC, data)
-        # generate fake mols to train the reward net (as augmentation; determining validity et al)
         n, e = session.run([model.nodes_gumbel_argmax, model.edges_gumbel_argmax],
                            feed_dict={model.training: False, model.embeddings: z_embeds})
         n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
         molsF = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
         rewardF = reward(molsF, METRIC, data)
 
-        feed_dict = {model.edges_labels: a,
-                     model.nodes_labels: x,
-                     model.embeddings: z_embeds,
-                     model.rewardR: rewardR,
-                     model.rewardF: rewardF,
-                     model.training: True,
-                     model.dropout_rate: DROPOUT,
-                     optimizer.la: la if epoch > 0 else 1.0}
-
-    else:
-        feed_dict = {model.edges_labels: a,
-                     model.nodes_labels: x,
-                     model.embeddings: z_embeds,
-                     model.training: True,
-                     model.dropout_rate: DROPOUT,
-                     optimizer.la: 1.0}
+        feed_dict.update({model.rewardR: rewardR,
+                          model.rewardF: rewardF})
 
     return feed_dict
 
@@ -72,26 +77,27 @@ def eval_fetch_dict(i, epochs, min_epochs, model, optimizer):
 
 
 def eval_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
-    mols, _, _, a, x, _, _, _, _ = data.next_validation_batch()
-    embeddings = model.sample_z(a.shape[0])
 
-    rewardR = reward(mols, METRIC, data)
+    molsR, _, _, a, x, _, _, _, _ = data.next_validation_batch(batch_dim)
 
+    z_embeds = model.sample_z(a.shape[0])
+
+    rewardR = reward(molsR, METRIC, data)
     n, e = session.run([model.nodes_gumbel_argmax, model.edges_gumbel_argmax],
-                       feed_dict={model.training: False, model.embeddings: embeddings})
+                       feed_dict={model.training: False, model.embeddings: z_embeds})
     n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
-    mols = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+    molsF = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+    rewardF = reward(molsR, METRIC, data)
 
-    record_mols_and_smiles(mols, i)
-
-    rewardF = reward(mols, METRIC, data)
+    record_mols_and_smiles(molsF, i)
 
     feed_dict = {model.edges_labels: a,
                  model.nodes_labels: x,
-                 model.embeddings: embeddings,
+                 model.embeddings: z_embeds,
                  model.rewardR: rewardR,
                  model.rewardF: rewardF,
                  model.training: False}
+
     return feed_dict
 
 
@@ -104,38 +110,66 @@ def _eval_update(i, epochs, min_epochs, model, optimizer, batch_dim, eval_batch)
 
 
 test_fetch_dict = eval_fetch_dict
-test_feed_dict = eval_feed_dict
 _test_update = _eval_update
 
 
+def test_feed_dict(i, epochs, min_epochs, model, optimizer, batch_dim):
+
+    molsR, _, _, a, x, _, _, _, _ = data.next_test_batch(batch_dim)
+
+    z_embeds = model.sample_z(a.shape[0])
+
+    rewardR = reward(molsR, METRIC, data)
+    n, e = session.run([model.nodes_gumbel_argmax, model.edges_gumbel_argmax],
+                       feed_dict={model.training: False, model.embeddings: z_embeds})
+    n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
+    molsF = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+    rewardF = reward(molsR, METRIC, data)
+
+    record_mols_and_smiles(molsF, i)
+
+    feed_dict = {model.edges_labels: a,
+                 model.nodes_labels: x,
+                 model.embeddings: z_embeds,
+                 model.rewardR: rewardR,
+                 model.rewardF: rewardF,
+                 model.training: False}
+
+    return feed_dict
+
+
 def record_mols_and_smiles(mols, epoch, draw=True):
-    smiles = list(map(lambda x: Chem.MolToSmiles(x) if MolecularMetrics.valid_lambda(x) else '', mols))
-    valid_unique_smiles = list(set([s for s in smiles if len(s) and '*' not in s and '.' not in s]))
+
+    smiles = list(map(lambda x: Chem.MolToSmiles(x) if MolecularMetrics.valid_lambda_special(x) else '',
+                      mols))
+
+    valid_unique_smiles = list(set([s for s in smiles if len(s) > 0]))
+
+    # introduce randomness for sure and then sample.
     np.random.shuffle(smiles)
     np.random.shuffle(valid_unique_smiles)
-
     smiles = smiles[:30]
     valid_unique_smiles = valid_unique_smiles[:30]
 
     with open(save_dir + '/sample.smiles', 'a') as fh:
-        fh.write('START Epoch %03d:\n' % epoch)
+        fh.write('\nSTART Epoch %03d:\n' % epoch)
         fh.write('\n'.join(smiles))
-        fh.write('END Epoch %03d:\n' % epoch)
+        fh.write('\nEND Epoch %03d:\n' % epoch)
     with open(save_dir + '/val_unique.smiles', 'a') as fh:
-        fh.write('START Epoch %03d:\n' % epoch)
+        fh.write('\nSTART Epoch %03d:\n' % epoch)
         fh.write('\n'.join(valid_unique_smiles))
-        fh.write('END Epoch %03d:\n' % epoch)
+        fh.write('\nEND Epoch %03d:\n' % epoch)
 
-    draw_mols = list(map(lambda x: Chem.MolFromSmiles(x) if len(x) else Chem.MolFromSmiles('C'), smiles))
-    draw_vu_mols = list(map(lambda x: Chem.MolFromSmiles(x), valid_unique_smiles))
+    mols = list(map(lambda x: Chem.MolFromSmiles(x), smiles))  # use CH4 for placeholder.
+    valid_unique_mols = list(map(lambda x: Chem.MolFromSmiles(x), valid_unique_smiles))
 
     if draw:
         from rdkit.Chem import Draw
-        if draw and len(draw_mols):
-            img = Draw.MolsToGridImage(draw_mols, molsPerRow=5, subImgSize=(300, 300))
+        if len(mols):
+            img = Draw.MolsToGridImage(mols, molsPerRow=5, subImgSize=(300, 300))
             img.save(save_dir + "/samples_%03d.png" % epoch)
-        if len(draw_vu_mols):
-            img = Draw.MolsToGridImage(draw_vu_mols, molsPerRow=5, subImgSize=(300, 300))
+        if len(valid_unique_mols):
+            img = Draw.MolsToGridImage(valid_unique_mols, molsPerRow=5, subImgSize=(300, 300))
             img.save(save_dir + "/val_unique_%03d.png" % epoch)
 
 
@@ -147,20 +181,9 @@ def Argparser():
     parser.add_argument('-m', '--model', type=str, help='model name.')
     parser.add_argument('-r', '--replicas', type=int, default=1, help='number of replicas of training.')
     parser.add_argument('--lam', type=float, default=0.05, help='lambda')
+    parser.add_argument('--cond_rate', type=float, default=0.30, help='top r proportion of nodes w.r.t. rewards'
+                                                                      'used in conditional training.')
     return parser
-
-
-MODELS = {  # Model, Discr, use_batch_discr, n_critics
-    'molgan': (GraphGANModel, encoder_rgcn, False, 5),
-    'molgan+batch': (GraphGANModel, encoder_rgcn, True, 3),
-    'pmax': (PacGANModel, encoder_rgcn, False, 3),
-    'pe1': (PacElas1Model, encoder_rgcn, False, 5),
-    'pe2': (PacElas2Model, encoder_rgcn, False, 5),
-    'pem': (PacMeanElas2Model, encoder_rgcn, False, 3),
-    'pmerge': (PacStatsMergeModel, encoder_rgcn, False, 3),
-    # 'gat': (GraphGANModel, encoder_gat, False),
-    # 'flat_gat': (GraphGANModel, encoder_flat_gat, False),
-}
 
 
 if __name__ == '__main__':
@@ -169,40 +192,43 @@ if __name__ == '__main__':
 
     LA = args.lam
 
-    Model, Discr, batch_discr, N_CRITIC = MODELS[args.model]
+    Model, discriminator, use_batch_discr, N_CRITIC = MODELS[args.model]
 
     for i in range(args.replicas):
 
-        tf.reset_default_graph()  # rebuild comp. graph across replicas
+        tf.reset_default_graph()  # rebuild graph across replicas
 
+        # load data.
         data = SparseMolecularDataset()
         data.load('data/gdb9_9nodes.sparsedataset')
 
-        steps = (len(data) // BATCH_DIM)
+        steps = (len(data) // BATCH_DIM)  # although weird (should be data.train_count // BATCH_DIM),
+                                          # we keep this setting the same as MolGAN.
 
+        # build result directory.
         save_dir = args.name + ('_%02d' % i if args.replicas > 1 else '')
         import os
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
-        # model
+        # model.
         model = Model(data.vertexes, data.bond_num_types, data.atom_num_types, Z_DIM,
                       decoder_units=DECODER_UNITS,
                       discriminator_units=DISCRIM_UNITS,
                       decoder=decoder_adj,
-                      discriminator=Discr,
+                      discriminator=discriminator,
                       soft_gumbel_softmax=False,
                       hard_gumbel_softmax=False,
-                      batch_discriminator=batch_discr)
+                      batch_discriminator=use_batch_discr)
 
-        # optimizer
+        # optimizer.
         optimizer = GraphGANOptimizer(model, learning_rate=1e-3, feature_matching=False)
 
-        # session
+        # session.
         session = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
         session.run(tf.global_variables_initializer())
 
-        # trainer
+        # trainer.
         trainer = Trainer(model, optimizer, session)
 
         print('Parameters: {}'.format(np.sum([np.prod(e.shape) for e in session.run(tf.trainable_variables())])))
@@ -214,8 +240,8 @@ if __name__ == '__main__':
                       _eval_update=_eval_update, _test_update=_test_update,
                       save_every=SAVE_EVERY, directory=save_dir)
 
+        # clean up
         session.close()
-
         del model, optimizer, trainer, data
 
 
